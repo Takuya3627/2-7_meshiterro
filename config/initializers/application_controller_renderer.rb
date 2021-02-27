abf239fd43010e61c79ea52ea35597a322289897
tm.0630.ppp@gmail.com <ec2-user@ip-172-31-30-131.ec2.internal>
2021-02-27 03:12:35 +0000

    Finish Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'cf3ea55b2c39b3b1ecc44494a45688c9247cccfe813f4d929fa6f7badd5b9be43e6e31fbf29affd571f2ed5dbf0f09667f00ddd96b87dd79b1561f7c98c25f1b'
